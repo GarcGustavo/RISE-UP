1a32185e399a65397bb9168434e32bbda056825c
Melvin Malave <melvin.malave@upr.edu>
2019-10-19 16:08:04 -0400

    Added user menu to header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <meta name="description" content>
         <meta name="author" content>
 
-        <title>Modern Business - Start Bootstrap Template</title>
+        <title>Interdisciplinary Research Network</title>
 
         <!-- Bootstrap core CSS -->
         <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
@@ -17,12 +17,12 @@
         <!-- Custom styles for this template -->
         <link href="{{asset('css/modern-business.css')}}" rel="stylesheet">
 
-
+        <!-- icons for this template -->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-        <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
-
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!-- font for this template -->
+        <link
+            href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900'
+            rel='stylesheet' type='text/css'>
 
     </head>
 
