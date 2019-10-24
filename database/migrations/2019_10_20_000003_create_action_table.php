611c976319e73d98eb3053bd7c4e9d831505d5b4
Gustavo Reyes <gustavo.reyes1@upr.edu>
2019-10-24 11:17:49 -0400

    added victor corrected migrations

---
+++ database/migrations/2019_10_20_000003_create_action_table.php
@@ -0,0 +1,34 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreateActionTable extends Migration
+{
+   
+    /**
+     * Run the migrations.
+     * @table action
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->Action, function (Blueprint $table) {
+
+            $table->bigIncrements('aid');
+            $table->date('a_date');
+            $table->foreign('a_type')->references('act_id')->on('action_type');
+            $table->foreign('a_user')->references('uid')->on('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {
+       Schema::dropIfExists($this->tableName);
+     }
+}
