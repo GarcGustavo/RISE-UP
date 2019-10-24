8ddf27cb23631ecaa61b08007d2d0d0fff04301f
GarcGustavo <gustavo.reyes1@upr.edu>
2019-10-23 22:10:24 -0400

    added auto-generated routes api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,32 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::resource('posts', 'PostAPIController');
+
+Route::resource('users', 'UserAPIController');
+
+Route::resource('actions', 'ActionAPIController');
+
+Route::resource('action__types', 'Action_TypeAPIController');
+
+Route::resource('cases', 'CaseAPIController');
+
+Route::resource('case__parameters', 'Case_ParametersAPIController');
+
+Route::resource('actions', 'ActionAPIController');
+
+Route::resource('c_s__parameters', 'CS_ParameterAPIController');
+
+Route::resource('groups', 'GroupAPIController');
+
+Route::resource('items', 'ItemAPIController');
+
+Route::resource('item__types', 'Item_TypeAPIController');
+
+Route::resource('options', 'OptionAPIController');
+
+Route::resource('roles', 'RoleAPIController');
+
+Route::resource('user__groups', 'User_GroupsAPIController');
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,64 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+
+Route::resource('posts', 'PostController');
+
+Route::resource('users', 'UserController');
+
+Route::resource('userGroups', 'User_GroupsController');
+
+Route::resource('roles', 'RoleController');
+
+Route::resource('options', 'OptionController');
+
+Route::resource('migrations', 'MigrationsController');
+
+Route::resource('itemTypes', 'Item_TypeController');
+
+Route::resource('items', 'ItemController');
+
+Route::resource('groups', 'GroupController');
+
+Route::resource('groups', 'GroupController');
+
+Route::resource('cSParameters', 'CS_ParameterController');
+
+Route::resource('caseParameters', 'Case_ParametersController');
+
+Route::resource('cases', 'CaseController');
+
+Route::resource('actionTypes', 'Action_TypeController');
+
+Route::resource('actions', 'ActionController');
+
+Route::resource('users', 'UserController');
+
+Route::resource('actions', 'ActionController');
+
+Route::resource('actionTypes', 'Action_TypeController');
+
+Route::resource('cases', 'CaseController');
+
+Route::resource('caseParameters', 'Case_ParametersController');
+
+Route::resource('actions', 'ActionController');
+
+Route::resource('cSParameters', 'CS_ParameterController');
+
+Route::resource('groups', 'GroupController');
+
+Route::resource('items', 'ItemController');
+
+Route::resource('itemTypes', 'Item_TypeController');
+
+Route::resource('options', 'OptionController');
+
+Route::resource('roles', 'RoleController');
+
+Route::resource('userGroups', 'User_GroupsController');
+
+Route::resource('users', 'UserController');
+
+Route::resource('users', 'UserController');
