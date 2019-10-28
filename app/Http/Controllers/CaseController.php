16c9355b07efe14c8fbbab890ecba44706a6cbda
GarcGustavo <gustavo.reyes1@upr.edu>
2019-10-28 12:47:59 -0400

    fixed case controller

--- app/Http/Controllers/CaseController.php
+++ app/Http/Controllers/CaseController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Case;
+use App\Http\Resources\Case_Study as CaseResource;
 
 class CaseController extends Controller
 {
