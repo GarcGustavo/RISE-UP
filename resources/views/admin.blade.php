23708786cd24e3b5bfa0b7a265a3a865d7690499
victor j. perez <victor.perez1@upr.edu>
2019-10-29 15:13:28 -0400

    users and requests nav tabs

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -19,12 +19,22 @@
             </ul>
         </div>
         <div class="col-lg-10 mt-3">
-            <div class="d-flex">
-                <ul class="list-inline mx-auto justify-content-center">
-                    <li class="list-inline-item">Users</li>
-                    <li class="list-inline-item">Request</li>
-                </ul>
+
+
+        <nav>
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                <a class="nav-item nav-link active" id="nav-users-tab" data-toggle="tab" href="#nav-users" role="tab" aria-controls="nav-users" aria-selected="true">Users</a>
+                <a class="nav-item nav-link" id="nav-requests-tab" data-toggle="tab" href="#nav-requests" role="tab" aria-controls="nav-requests" aria-selected="false">Requests</a>
             </div>
+        </nav>
+        <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-users" role="tabpanel" aria-labelledby="nav-users-tab">
+                list users here
+            </div>
+            <div class="tab-pane fade" id="nav-requests" role="tabpanel" aria-labelledby="nav-requests-tab">list requests here</div>
+        </div>
+
+
             <div>@yield('adminTable')</div>
         </div>
     </div>
