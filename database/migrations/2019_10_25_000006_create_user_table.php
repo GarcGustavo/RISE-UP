c703a9cbd6b30901e24d77ca1223904f2b709d36
victor j. perez <victor.perez1@upr.edu>
2019-11-11 11:20:05 -0400

    user migration

---
+++ database/migrations/2019_10_25_000006_create_user_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+class CreateUserTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('User', function (Blueprint $table) {
+            $table->bigIncrements('uid');
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->string('email', 255);
+            $table->string('contact_email', 255);
+            $table->date('u_creation_date');
+            $table->date('u_expiration_date');
+            $table->boolean('u_ban_status');
+            $table->boolean('u_role_upgrade_request');
+            $table->string('current_edit_cid', 255);
+            $table->unsignedBigInteger('u_role');
+            $table->foreign('u_role')->references('rid')->on('Role');
+            $table->softDeletes();
+        });
+    }
+
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('User');
+    }
+}
