4d80ced2cd242880c7d74205389878602ffec170
victor j. perez <victor.perez1@upr.edu>
2019-12-01 08:58:14 -0400

    only one action per day per user on user actions controller

--- app/Http/Controllers/AdminUsersActionsController.php
+++ app/Http/Controllers/AdminUsersActionsController.php
@@ -14,18 +14,24 @@ class AdminUsersActionsController extends Controller
     public function index(){
         $latestDate = DB::table('Action')
             ->select('a_user', DB::raw('MAX(a_date) as latest_action_date'))
-            ->groupBy('a_user')
-            ->orderBy('latest_action_date', 'desc');
+            ->groupBy('a_user');
+            //->orderBy('latest_action_date', 'desc');
             //->get();
+			
+        $ooa = DB::table('Action')
+            ->select('a_user', 'a_date', DB::raw('MAX(a_type) as recent_action_type'))
+            ->groupBy('a_user')
+			->groupBy('a_date');
+            //->orderBy('recent_action_type', 'desc');			
 
         $users = DB::table('User')
             ->joinSub($latestDate, 'latestDate','latestDate.a_user', '=', 'User.uid')
 
-            ->join('Action',  function ($join) {
-                $join->on('Action.a_user', '=', 'latestDate.a_user')
-                     ->on('Action.a_date', '=', 'latestDate.latest_action_date');
+            ->joinSub($ooa, 'ooa',  function ($join) {
+                $join->on('ooa.a_user', '=', 'latestDate.a_user')
+                     ->on('ooa.a_date', '=', 'latestDate.latest_action_date');
             })
-            ->join('Action_Type', 'Action_Type.act_id', '=', 'Action.a_type')
+            ->join('Action_Type', 'Action_Type.act_id', '=', 'ooa.recent_action_type')
             ->orderBy('latest_action_date', 'desc')
             ->get();
 
@@ -52,3 +58,4 @@ class AdminUsersActionsController extends Controller
 
     }
 }
+//show only one per user
