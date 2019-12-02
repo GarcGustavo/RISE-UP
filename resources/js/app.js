2f6bffb9dc9497268bd7dc28cb9fc72298243b46
Elnois Norat <elnois.norat@gmail.com>
2019-12-02 17:03:38 -0400

    2 dic 2019 push

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,6 @@
  */
 //import JwPagination from 'jw-vue-pagination';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 34140abe639af7d6b49c39f387b701dd525ddd1d
 require('./bootstrap');
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -31,18 +27,6 @@ Vue.component('paginator', require('./components/paginator.vue').default);
 Vue.component('action-table-dbox', require('./components/dialogue-boxes/action-table-dbox.vue').default);
 Vue.component('case-create-dbox', require('./components/dialogue-boxes/case-create-dbox.vue').default);
 //web pages
-<<<<<<< HEAD
-Vue.component('iren_header', require('./components/header.vue').default);
-Vue.component('iren_footer', require('./components/footer.vue').default);
-Vue.component('iren_help', require('./components/help.vue').default);
-Vue.component('iren_about', require('./components/about.vue').default);
-Vue.component('iren_group', require('./components/group.vue').default);
-Vue.component('iren_user_groups', require('./components/user_groups.vue').default);
-Vue.component('iren_user_cases', require('./components/user_cases.vue').default);
-Vue.component('iren_login', require('./components/login.vue').default);
-Vue.component('iren_home', require('./components/home.vue').default);
-Vue.component('iren_profile', require('./components/profile.vue').default);
-=======
 Vue.component('iren-header', require('./components/header.vue').default);
 Vue.component('iren-footer', require('./components/footer.vue').default);
 Vue.component('iren-help', require('./components/help.vue').default);
@@ -54,7 +38,6 @@ Vue.component('iren-404', require('./components/errors/404-error.vue').default);
 Vue.component('case_study', require('./components/case_study.vue').default);
 Vue.component('home', require('./components/home.vue').default);
 Vue.component('items', require('./components/items.vue').default);
->>>>>>> 34140abe639af7d6b49c39f387b701dd525ddd1d
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
