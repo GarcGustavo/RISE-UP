fc2c6baf2049e8425f7d1283a4a3bdf5dc52713a
victor j. perez <victor.perez1@upr.edu>
2019-12-02 01:56:52 -0400

    option parameter o category cspid

--- resources/views/admin/filters/addOptionForm.blade.php
+++ resources/views/admin/filters/addOptionForm.blade.php
@@ -3,7 +3,7 @@
     @csrf
     <div class="d-flex flex-row">
         <button type="submit" class="btn btn-dark m-2">Add Filter</button>
-		<input type="hidden" id="o_parameter" name="o_parameter" value={{$option->o_parameter}}>
+		<input type="hidden" id="o_parameter" name="o_parameter" value={{$category->csp_id}}>
         <input class="m-2" type="text" id="o_content" name="o_content" placeholder="New Filter">
     </div>
     @if($errors->has('o_content'))
