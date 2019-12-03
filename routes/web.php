9fc13ba10811c281e50a1cebbd451b3d2a68b51b
victor j. perez <victor.perez1@upr.edu>
2019-12-03 14:26:31 -0400

    editing routes

--- routes/web.php
+++ routes/web.php
@@ -179,92 +179,102 @@ Route::post('/parameter/update', 'Case_ParametersController@updateCaseParameters
 //Create default parameters in a case
 Route::post('/parameter/create/defaults', 'Case_ParametersController@createDefaultParameters');
 
+
+/********ADMIN DASHBOARD*********/
+
 //Admin board
-//Software Requirement 2.60. The web application will allow an Admin to view the “Admin Dashboard” page
+//Software Requirement 2.58. The web application will allow an Admin to view the “Admin Dashboard” page
 
 
 //Provides the admin with a list of users.
-//Software Requirement 2.62 on Software Requirement Specifications Documents
+//Software Requirement 2.60 on Software Requirement Specifications Documents
 Route::get('/admin/users-requests', 'AdminUsersRequestsController@index')->name('admin.users-requests');
 
 
 //Provides the admin with a list of users
 //requesting their role to be upgraded from Viewer to Collaborator.
-//Software Requirement 2.64 on Software Requirement Specifications Documents
+//Software Requirement 2.62 on Software Requirement Specifications Documents
 //Route::get('/admin/users-requests', 'AdminUsersRequestsController@index')->name('admin.users-requests');
 
 
+//Admin approve the user requests, and edits a User
+//Software Requirement 2.67. The web application will allow an Admin to approve the user requests for Collaborator role permissions.
+//Software Requirement 2.74. The web application will allow an Admin to ban a Collaborator, removing their role permissions.
+//Software Requirement 2.75. The web application will allow an Admin to unban a Collaborator, restoring their role permissions.
+Route::get('/admin/user/{id}/edit', 'AdminUserController@edit');
+
+
+//Admin approve the user requests, and edits a User
+//Software Requirement 2.67. The web application will allow an Admin to approve the user requests for Collaborator role permissions.
+//Software Requirement 2.74. The web application will allow an Admin to ban a Collaborator, removing their role permissions.
+//Software Requirement 2.75. The web application will allow an Admin to unban a Collaborator, restoring their role permissions.
+Route::put('/admin/user/{id}', 'AdminUserController@update');
+
+
 //Presents latest action for each user
-//User Requirement 2.61. The web application will allow an Admin to view the recent activity of users
+//User Requirement 2.59. The web application will allow an Admin to view the recent activity of users
 //and groups in the system, the activities are:
 //1. Creation of case studies   2. Updates to case studies   3. Deletion of case studies
 Route::get('/admin/users-actions', 'AdminUsersActionsController@index')->name('admin.users-actions');
 
 
 //Presents recent actions for a particular user
-//Software Requirement 2.61. The web application will allow an Admin to view the recent activity of users
+//Software Requirement 2.59. The web application will allow an Admin to view the recent activity of users
 //and groups in the system, the activities are:
 //1. Creation of case studies   2. Updates to case studies   3. Deletion of case studies
 Route::get('/admin/users-actions/{id}', 'AdminUsersActionsController@show');
 
 
 //Presents group name, the group owner, the latest action, and creation date for each group.
-//Software Requirement 2.63. The web application will allow an Admin to view the groups of the system.
+//Software Requirement 2.61. The web application will allow an Admin to view the groups of the system.
 Route::get('/admin/groups', 'AdminGroupsController@index')->name('admin.groups');
 
 
 //Presents recent actions for a particular group
-//Software Requirement 2.61. The web application will allow an Admin to view the recent activity of users
+//Software Requirement 2.59. The web application will allow an Admin to view the recent activity of users
 //and groups in the system, the activities are:
 //1. Creation of case studies   2. Updates to case studies   3. Deletion of case studies
 Route::get('/admin/groups-actions/{id}', 'AdminGroupsActionsController@show');
 
 
 //List filters
-//Software Requirement 2.65  The web application will allow an Admin to view a list of search filters for case studies.
-//Software Requirement 2.67  The web application will allow an Admin to view a list of search filter categories for case studies.
+//Software Requirement 2.63  The web application will allow an Admin to view a list of search filters for case studies.
+//Software Requirement 2.65  The web application will allow an Admin to view a list of search filter categories for case studies.
+//Software Requirement 2.64  The web application will allow an Admin to view a list of default search filters for case studies.
+//Software Requirement 2.66  The web application will allow an Admin to view a list of default search filter categories for case studies.
 Route::get('/admin/filters', 'AdminFiltersController@index')->name('admin.filters');
 
 
 //Add new filter
-//User Requirement: 2.70.  The web application will allow an Admin to add new filters to the list of search filters for case studies.
+//User Requirement: 2.68.  The web application will allow an Admin to add new filters to the list of search filters for case studies.
 Route::post('/admin/filter-option', 'AdminFilterOptionController@store');
 
-
-//Remove filter
-//Software Requirement: 2.74.  The web application will allow an Admin to remove search filters they have created for case studies
-Route::delete('/admin/filter-option/{id}', 'AdminFilterOptionController@destroy');
-
-//Edit filter 
-//User Requirement: (2.xx)  The web application will allow an Admin to modify search filter they have created for case studies
-Route::put('/admin/filter-option/{id}', 'AdminFilterOptionController@update');
-
 //Add new filter category
-//Software Requirement: 2.71.  The web application will allow an Admin to add new categories to the list of search filters for case studies.
+//Software Requirement: 2.69.  The web application will allow an Admin to add new categories to the list of search filters for case studies.
 Route::post('/admin/filter-category', 'AdminFilterCategoryController@store');
 
 
-//Remove filter category
-//Software Requirement: 2.75.  The web application will allow an Admin to remove search filter categories they have created for case studies
-Route::delete('/admin/filter-category/{id}', 'AdminFilterCategoryController@destroy');
-
+//Edit filter 
+//User Requirement: (2.70)  The web application will allow an Admin to modify search filter they have created for case studies
+Route::put('/admin/filter-option/{id}', 'AdminFilterOptionController@update');
 
 //Edit filter category
-//User Requirement: (2.xx)  The web application will allow an Admin to modify search filter categories they have created for case studies
+//User Requirement: (2.71)  The web application will allow an Admin to modify search filter categories they have created for case studies
 Route::put('/admin/filter-category/{id}', 'AdminFilterCategoryController@update');
 
 
-//Admin Edits a User
-//Software Requirement 2.69. The web application will allow an Admin to approve the user requests for Collaborator role permissions.
-//Software Requirement 2.76. The web application will allow an Admin to ban a Collaborator, removing their role permissions.
-//Software Requirement 2.77. The web application will allow an Admin to unban a Collaborator, restoring their role permissions.
-Route::get('/admin/user/{id}/edit', 'AdminUserController@edit');
+//Remove filter
+//Software Requirement: 2.72.  The web application will allow an Admin to remove search filters they have created for case studies
+Route::delete('/admin/filter-option/{id}', 'AdminFilterOptionController@destroy');
+
+//Remove filter category
+//Software Requirement: 2.73.  The web application will allow an Admin to remove search filter categories they have created for case studies
+Route::delete('/admin/filter-category/{id}', 'AdminFilterCategoryController@destroy');
 
 
-//Admin Edits a User
-//Software Requirement 2.69. The web application will allow an Admin to approve the user requests for Collaborator role permissions.
-//Software Requirement 2.76. The web application will allow an Admin to ban a Collaborator, removing their role permissions.
-//Software Requirement 2.77. The web application will allow an Admin to unban a Collaborator, restoring their role permissions.
-Route::put('/admin/user/{id}', 'AdminUserController@update');
+
+
+
+
 
 
